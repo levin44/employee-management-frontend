70d6be3723caac4507136799113ed24ed8eda900
levin44 <nisaldayan@gmail.com>
2024-07-06 16:59:35 +0530

    2024-06-27.mp4 end

---
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-manage-emp',
+  standalone: true,
+  imports: [FormsModule,HttpClientModule,CommonModule],
+  templateUrl: './manage-emp.component.html',
+  styleUrl: './manage-emp.component.css'
+})
+export class ManageEmpComponent {
+  public employeeObj={
+    firstName: '',
+    lastName: '',
+    email: '',
+    departmentId: '',
+    roleId: ''
+  }
+  constructor(private http:HttpClient) { }
+
+  addEmployee(){
+this.http.post("http://localhost:8080/emp-controller/add-employee",this.employeeObj).subscribe((res)=>{
+  console.log(res);
+  
+})
+  }
+
+}
