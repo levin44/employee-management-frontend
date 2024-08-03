3ba23218e6cece6c5129b14474c0503d365a274c
levin44 <nisaldayan@gmail.com>
2024-08-03 22:05:44 +0530

    fixed update

--- src/app/page/view-all-employee/view-all-employee.component.ts
+++ src/app/page/view-all-employee/view-all-employee.component.ts
@@ -8,63 +8,98 @@ import { NavComponent } from '../../common/nav/nav.component';
 @Component({
   selector: 'app-view-all-employee',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule,NavComponent],
+  imports: [HttpClientModule, FormsModule, CommonModule, NavComponent],
   templateUrl: './view-all-employee.component.html',
   styleUrl: './view-all-employee.component.css'
 })
 export class ViewAllEmployeeComponent {
-  public employeeList:any;
-  constructor(private http:HttpClient) {
+  public employeeList: any;
+
+  public selectedEmployee: any ={
+    "id":null,
+    "firstName":null,
+    "lastName":null,
+    "email":null,
+    "departmentId":null,
+    "roleId":null
+  };
+
+  constructor(private http: HttpClient) {
     this.loadTable();
   }
-loadTable(){
-  this.http.get("http://localhost:8080/emp-controller/get-all").subscribe((res)=>{
-    this.employeeList=res;
-    console.log(res);
-  })
-}
+  loadTable() {
+    this.http.get("http://localhost:8080/emp-controller/get-all").subscribe((res) => {
+      this.employeeList = res;
+      console.log(res);
+    })
+  }
 
-deleteEmployee(id:any){
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: "btn btn-success",
-      cancelButton: "btn btn-danger"
-    },
-    buttonsStyling: false
-  });
-  swalWithBootstrapButtons.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonText: "Yes, delete it!",
-    cancelButtonText: "No, cancel!",
-    reverseButtons: true
-  }).then((result) => {
-    if (result.isConfirmed) {
-      this.http.delete(`http://localhost:8080/emp-controller/delete-emp/${id}`, {responseType: "text"}).subscribe((res)=>{
-        this.loadTable();
-        console.log(res);
+  deleteEmployee(id: any) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.delete(`http://localhost:8080/emp-controller/delete-emp/${id}`, { responseType: "text" }).subscribe((res) => {
+          this.loadTable();
+          console.log(res);
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        })
+
+
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
         swalWithBootstrapButtons.fire({
-          title: "Deleted!" ,
-          text: "Your file has been deleted.",
-          icon: "success"
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
         });
-      })
+      }
+    });
+  }
 
-    
-    } else if (
-      /* Read more about handling dismissals below */
-      result.dismiss === Swal.DismissReason.cancel
-    ) {
-      swalWithBootstrapButtons.fire({
-        title: "Cancelled",
-        text: "Your imaginary file is safe :)",
-        icon: "error"
-      });
+
+
+  updateEmployee(employee: any) {
+    this.selectedEmployee = employee;
     }
-  });
-    
 
-}
+    saveUpdateEmployee(){
+      Swal.fire({
+        title: "Do you want to save the changes?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        denyButtonText: `Don't save`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.http.put("http://localhost:8080/emp-controller/update-employee",this.selectedEmployee).subscribe((res)=>{
+            console.log(res); 
+          })
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
+
+    }
 }
